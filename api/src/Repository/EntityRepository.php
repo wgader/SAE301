8308e15f00df1152bc457e3d7d684805da4d4932
Wahel Gader <wahel.gader@etu.unilim.fr>
2025-10-15 11:19:20 +0200

    suppression branch

--- api/src/Repository/EntityRepository.php
+++ api/src/Repository/EntityRepository.php
@@ -21,9 +21,9 @@ abstract class EntityRepository {
 
     protected function __construct(){
         // Modifiez ici vos informations de BDD et de connexion
-        $this->cnx = new PDO("mysql:host=127.0.0.1:8889;dbname=SAE301", "root", "root");
+        $this->cnx = new PDO("mysql:host=127.0.0.1:3306;dbname=gader3", "gader3", "gader3");
     }
-
+    // 
     /**
      *  find
      *  Query the data base for the entity with id $id.
